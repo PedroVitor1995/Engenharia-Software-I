fa700afa17bf5fcee93250223357145d998c7b5a
Pedro Vitor <pedrov6@outlook.com>
2019-10-17 17:38:42 -0300

    criação de layouts

--- Gerenciador_Tarefas/eng/app/models.py
+++ Gerenciador_Tarefas/eng/app/models.py
@@ -7,8 +7,15 @@ class Aluno(models.Model):
 
 	def __str__(self):
 		return self.nome
+		
+class Grupo(models.Model):
+	descricao = models.CharField(max_length=300)
 
 class Tarefa(models.Model):
 	descricao = models.CharField(max_length=300)
 	data_realizacao = models.DateField()
-	aluno = models.ForeignKey(Aluno, related_name='alunos',on_delete="CASCADE")
+	aluno = models.ForeignKey(Aluno, related_name='aluno',on_delete="CASCADE")
+	grupo = models.ForeignKey(Grupo, related_name='grupo',on_delete="CASCADE")
+
+
+
